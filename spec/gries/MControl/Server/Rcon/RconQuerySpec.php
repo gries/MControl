567d1379cf05c642880ed599c1fd3e25c820ec15
gries <christoph@rosse.at>
2013-11-10 22:34:33 +0100

    Removed dead code

--- spec/gries/MControl/Server/Rcon/RconQuerySpec.php
+++ spec/gries/MControl/Server/Rcon/RconQuerySpec.php
@@ -12,11 +12,4 @@ class RconQuerySpec extends ObjectBehavior
         $this->shouldBeAnInstanceOf('gries\MControl\Server\Rcon\RconQuery');
         $this->shouldBeAnInstanceOf('\SourceQuery');
     }
-
-//    function it_should_tell_us_about_its_connect_state()
-//    {
-//        $this->setConnected(true);
-//
-//        $this->isConnected()->shouldBe(true);
-//    }
 }
