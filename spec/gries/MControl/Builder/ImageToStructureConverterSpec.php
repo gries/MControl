e74a31aa5d67449f90cca41fd0c0ebde4c0847f6
gries <christoph@rosse.at>
2013-11-16 19:07:16 +0100

    Define ImageToStrctureConverter Specification.

---
+++ spec/gries/MControl/Builder/ImageToStructureConverterSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\gries\MControl\Builder;
+
+use gries\MControl\Builder\Structure;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ImageToStructureConverterSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('gries\MControl\Builder\ImageToStructureConverter');
+    }
+
+    function it_should_create_a_structure_based_on_a_Imagick_object()
+    {
+        $expectedStructure = new Structure();
+        $expectedStructure->createBlock('iron_block', array('x' =>1, 'y' => 1, 'z' => 1));
+        $expectedStructure->createBlock('iron_block', array('x' =>2, 'y' => 1, 'z' => 1));
+
+        $expectedStructure->createBlock('coal_block', array('x' =>1, 'y' => 1, 'z' => 2));
+        $expectedStructure->createBlock('coal_block', array('x' =>2, 'y' => 1, 'z' => 2));
+
+        // returns a 2x2 image with 2 black and 2 white pixels
+        $image = $this->getImage();
+
+        $this->setWhiteBlockType('iron_block');
+        $this->setBlackBlockType('coal_block');
+
+        $this->convert($image)
+            ->shouldBeLike($expectedStructure)
+        ;
+    }
+
+    protected function getImage()
+    {
+        $image = new \Imagick();
+        $image->newImage(2,2, new \ImagickPixel('white'));
+
+        $draw = new \ImagickDraw();
+        $draw->setFillColor(new \ImagickPixel('black'));
+
+        $draw->color(1, 2, \Imagick::PAINT_POINT );
+
+        $image->drawImage( $draw );
+    }
+}
