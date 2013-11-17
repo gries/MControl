6bab199eb35ccc4551e01f9d59ef679163e4e3a0
gries <christoph@rosse.at>
2013-11-17 18:07:10 +0100

    Add phpdoc.

--- src/gries/MControl/Storage/StructureRepositoryFactory.php
+++ src/gries/MControl/Storage/StructureRepositoryFactory.php
@@ -1,12 +1,18 @@
 <?php
 namespace gries\MControl\Storage;
 
-
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
 class StructureRepositoryFactory
 {
+    /**
+     * Create a new structure repository
+     *
+     * @param EntityManager $entityManager
+     *
+     * @return StructureRepository
+     */
     public function create(EntityManager $entityManager)
     {
         $storage = new DoctrineStructureStorage($entityManager);
