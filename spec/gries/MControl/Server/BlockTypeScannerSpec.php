8d79a2db917a87f56866845f795096e9c3def81b
gries <christoph@rosse.at>
2013-11-24 17:04:26 +0100

    BLockTypeScanner now detects meta information.

--- spec/gries/MControl/Server/BlockTypeScannerSpec.php
+++ spec/gries/MControl/Server/BlockTypeScannerSpec.php
@@ -39,6 +39,12 @@ class BlockTypeScannerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn(true);
 
+        $commander
+            ->testForBlock($coordinates, $dummyBlockType, 0)
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
+
         $repo->getByName('minecraft:air')
             ->shouldBeCalled()
             ->willReturn($dummyBlockType);
@@ -52,10 +58,43 @@ class BlockTypeScannerSpec extends ObjectBehavior
      * @param gries\MControl\Server\Commander                      $commander
      * @param gries\MControl\Storage\BlockType\BlockTypeRepository $repo
      */
+    function it_detects_a_blocktype_with_correct_meta_information(Commander $commander, BlockTypeRepository $repo)
+    {
+        $dummyBlockType = new BlockType(array('name' => 'air'));
+        $expectedBlockType = new BlockType(array('name' => 'air', 'meta' => 4));
+
+        $coordinates = array('x' => 1, 'y' => 1, 'z' => 1);
+
+        $commander
+            ->testForBlock($coordinates, $dummyBlockType)
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
+
+        $commander
+            ->testForBlock($coordinates, $dummyBlockType, 0)
+            ->shouldBeCalled()
+            ->willReturn(4)
+        ;
+
+        $repo->getByName('minecraft:air')
+            ->shouldBeCalled()
+            ->willReturn($dummyBlockType);
+
+        $this
+            ->detectBlockType($coordinates)
+            ->shouldBeLike($expectedBlockType);
+    }
+
+    /**
+     * @param gries\MControl\Server\Commander                      $commander
+     * @param gries\MControl\Storage\BlockType\BlockTypeRepository $repo
+     */
     function it_searches_the_repo_if_type_is_not_detected(Commander $commander, BlockTypeRepository $repo)
     {
         $dummyBlockType = new BlockType(array('name' => 'air'));
         $dirtType       = new BlockType(array('name' => 'dirt'));
+        $expectedtype   = new BlockType(array('name' => 'dirt', 'meta' => 0));
 
         $coordinates = array('x' => 1, 'y' => 1, 'z' => 1);
 
@@ -72,9 +111,15 @@ class BlockTypeScannerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($dirtType);
 
+        $commander
+            ->testForBlock($coordinates, $dirtType, 0)
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
+
         $this
             ->detectBlockType($coordinates)
-            ->shouldReturn($dirtType);
+            ->shouldBeLike($expectedtype);
     }
 
     /**
@@ -85,7 +130,7 @@ class BlockTypeScannerSpec extends ObjectBehavior
     {
         $dummyBlockType = new BlockType(array('name' => 'air'));
         $plankType      = new BlockType(array('name' => 'minecraft:planks', 'title' => 'Planks'));
-
+        $expectedType   = new BlockType(array('name' => 'minecraft:planks', 'title' => 'Planks', 'meta' => 0));
         $coordinates = array('x' => 1, 'y' => 1, 'z' => 1);
 
         $commander
@@ -105,9 +150,15 @@ class BlockTypeScannerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn($plankType);
 
+        $commander
+            ->testForBlock($coordinates, $plankType, 0)
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
+
         $this
             ->detectBlockType($coordinates)
-            ->shouldReturn($plankType);
+            ->shouldBeLike($expectedType);
     }
 
     /**
@@ -118,6 +169,7 @@ class BlockTypeScannerSpec extends ObjectBehavior
     {
         $dummyBlockType = new BlockType(array('name' => 'air'));
         $plankType      = new BlockType(array('name' => 'minecraft:planks', 'title' => 'Wooden Planks'));
+        $expectedType   = new BlockType(array('name' => 'minecraft:planks', 'title' => 'Wooden Planks', 'meta' => 0));
 
         $airBlockType  = $dummyBlockType;
         $ironBlockType = new BlockType(array('name' => 'minecraft:iron_ore'));
@@ -165,9 +217,14 @@ class BlockTypeScannerSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn(true);
 
+        $commander
+            ->testForBlock($coordinates, $plankType, 0)
+            ->shouldBeCalled()
+            ->willReturn(true);
+
         $this
             ->detectBlockType($coordinates)
-            ->shouldReturn($plankType)
+            ->shouldBeLike($expectedType)
         ;
     }
 }
--- src/gries/MControl/Server/BlockTypeScanner.php
+++ src/gries/MControl/Server/BlockTypeScanner.php
@@ -2,6 +2,7 @@
 
 namespace gries\MControl\Server;
 
+use gries\MControl\Builder\BlockType;
 use gries\MControl\Storage\BlockType\BlockTypeRepository;
 
 class BlockTypeScanner
@@ -23,26 +24,38 @@ class BlockTypeScanner
         $detectedType = $this->commander->testForBlock($coordinates, $dummyType);
 
         if (true === $detectedType) {
-            return $dummyType;
+            return $this->detectMeta($coordinates, $dummyType);
         }
 
         // search by name
         if ($type = $this->repository->getByName($detectedType)) {
-            return $type;
+            return $this->detectMeta($coordinates, $type);
         }
 
         // search by title
         if ($type = $this->repository->getByTitle($detectedType)) {
-            return $type;
+            return $this->detectMeta($coordinates, $type);
         }
 
         // cant find it so bruteforce
         foreach ($this->repository->getAll() as $type) {
 
             if (true === $this->commander->testForBlock($coordinates, $type)) {
-                return $type;
+                return $this->detectMeta($coordinates, $type);
             }
         }
+    }
+
+    protected function detectMeta($coordinates, BlockType $type)
+    {
+        if (true === $meta = $this->commander->testForBlock($coordinates, $type, 0)) {
+            $type->updateData(array('meta' => 0));
+        }
+        else {
+            $type->updateData(array('meta' => $meta));
+        }
+
+        return $type;
     }
 
     protected function getDummyBlockType()
